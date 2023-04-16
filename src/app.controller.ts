705221854570cb4eba85580e0b7ab5af693837d3
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2023-04-16 18:51:32 +0900

    add dummy

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,13 @@
-import { Get, Controller } from '@nestjs/common';
+import { Get, Controller, Header } from '@nestjs/common';
 
 @Controller()
 export class AppController {
   @Get()
+  @Header('content-type', 'text/html')
   root(): string {
-    return 'Hello World!';
+    return  Object
+      .entries({hello: "hello world", bye: "bye world"})
+      .map(([key, value]) => `key(${key}): value(${value})`)
+      .join('\n');
   }
 }
